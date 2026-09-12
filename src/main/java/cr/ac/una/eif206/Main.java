6d6dfadd6dcff9b2651f8b6724e8c9acb24fff31
Montoya <jordana.montoya.garita@est.una.ac.cr>
2026-09-12 15:01:49 -0600

    Modificación del main para observar como va la estructura

--- src/main/java/cr/ac/una/eif206/Main.java
+++ src/main/java/cr/ac/una/eif206/Main.java
@@ -4,15 +4,21 @@ import cr.ac.una.eif206.presentacion.controlador.LoginController;
 import cr.ac.una.eif206.presentacion.vista.LoginView;
 
 import javax.swing.SwingUtilities;
-
-
-//Punto de entrada del programa.
+import javax.swing.UIManager;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Se usa invokeLater para que la interfaz grafica se cree en el
-        // "Event Dispatch Thread" de Swing.
+
+        // Intenta utilizar la apariencia visual del sistema operativo.
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            System.out.println("No se pudo cargar la apariencia del sistema.");
+        }
+        //SwingUtilities.invokeLater hace que toda la interfaz gráfica se ejecute en el hilo recomendado por Java Swing.
+
         SwingUtilities.invokeLater(() -> {
             LoginView loginView = new LoginView();
             new LoginController(loginView);
